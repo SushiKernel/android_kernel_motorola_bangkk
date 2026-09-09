d084d45812ada656917ebd8ff5163462c274fea7
whyakari <barryofc11@gmail.com>
2026-09-09 21:49:11 +0000

    fs: stat: Remove deprecated manual hook return calls
    Drop ksu_handle_newfstat_ret and ksu_handle_fstat64_ret that are
    incompatible with CONFIG_KSU_SUSFS_INLINE_HOOK.
    
    Signed-off-by: whyakari <barryofc11@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -390,13 +390,7 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 #ifdef CONFIG_KSU
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user,
 				int *flags);
-
-extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
-#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
-extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr); // optional
 #endif
-#endif
-
 #if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)
 SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 		struct stat __user *, statbuf, int, flag)
@@ -420,9 +414,6 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
-#ifdef CONFIG_KSU
-	ksu_handle_newfstat_ret(&fd, &statbuf);
-#endif
 	return error;
 }
 #endif
@@ -549,9 +540,6 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
-#ifdef CONFIG_KSU // for 32-bit
-	ksu_handle_fstat64_ret(&fd, &statbuf);
-#endif
 	return error;
 }
 
