e32c9feded49772483b7f52f217702fa5b0b3149
whyakari <barryofc11@gmail.com>
2026-09-09 17:59:09 -0300

    susfs: Apply full 5.4 patch with inline hook integration
    Integrate complete SUSFS 5.4 patch, fix fd.c hunk for SUS_MOUNT
    and OPEN_REDIRECT support, remove deprecated manual hook symbols,
    and add CONFIG_KSU_SUSFS_INLINE_HOOK guards.
    
    Signed-off-by: whyakari <barryofc11@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -30,6 +30,13 @@ extern void susfs_sus_kstat_spoof_generic_fillattr(struct inode *inode, struct k
 extern int susfs_get_non_sus_mnt_id_from_mnt(struct mount *orig_mnt);
 #endif
 
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+extern void susfs_sus_kstat_spoof_generic_fillattr(struct inode *inode, struct kstat *stat);
+#endif
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+extern int susfs_get_non_sus_mnt_id_from_mnt(struct mount *orig_mnt);
+#endif
+
 /**
  * generic_fillattr - Fill in the basic attributes from the inode struct
  * @inode: Inode to use as the source
